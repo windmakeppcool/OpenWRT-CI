daad3551ed7733fbc06dcd2c4954ff548c1ba5b0
lly <lly@faw.com>
2024-06-25 14:15:02 +0800

    add istore and tailscale

---
+++ Scripts/Appendles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PKG_PATCH="$GITHUB_WORKSPACE/wrt/package/"
+
+# luci-app-tailscale
+sed -i '/\/etc\/init\.d\/tailscale/d;/\/etc\/config\/tailscale/d;' feeds/packages/net/tailscale/Makefile
+git clone https://github.com/asvow/luci-app-tailscale package/luci-app-tailscale
+
